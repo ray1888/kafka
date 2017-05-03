a67f6339b02f40638693ff25a9e7f0cddd4ab3cc
布小沫 <95112082@qq.com>
2017-05-04 00:13:16 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,11 @@
 sed -i "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/0.0.0.0:9092/g" config/server.properties
 sed -i "s/#advertised.listeners/advertised.listeners/g" config/server.properties
 sed -i "s/your.host.name:9092/${KAFKA_HOST_IP}:${KAFKA_PORT}/g" config/server.properties
-bin/zookeeper-server-start.sh config/zookeeper.properties & 
+if [ ${ZOOKEEPER} == "localhost:2181" ];
+  bin/zookeeper-server-start.sh config/zookeeper.properties & 
+else 
+  sed -i "s/zookeeper.connect=localhost:2181/zookeeper.connect=${ZOOKEEPER}/g" config/server.properties
+if
 bin/kafka-server-start.sh config/server.properties & 
 sleep 5
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ${KAFKA_TOPICS}
