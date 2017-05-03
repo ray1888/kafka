87ceec91e3a6556af2ec208b8d4b4374cd2d2897
布小沫 <95112082@qq.com>
2017-05-03 15:20:49 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sed -i "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/0.0.0.0:9092/g" config/server.properties
 sed -i "s/#advertised.listeners/advertised.listeners/g" config/server.properties
-sed -i "s/your.host.name:9092/${HOST_IP}:9092/g" config/server.properties
+sed -i "s/your.host.name:9092/${KAFKA_HOST_IP}:${KAFKA_PORT}/g" config/server.properties
 bin/zookeeper-server-start.sh config/zookeeper.properties & 
 bin/kafka-server-start.sh config/server.properties & 
 sleep 5
-bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ${TOPICS}
+bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ${KAFKA_TOPICS}
 tail -f logs/*.log
