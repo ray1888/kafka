560c7bc7b7221c0b152e0d07e5e2f9b9df6616a2
布小沫 <95112082@qq.com>
2017-05-03 10:18:55 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "advertised.listeners=PLAINTEXT://10.2.23.3:9092" >> config/server.properties
+echo "advertised.listeners=PLAINTEXT://10.2.23.3:4005" >> config/server.properties
 bin/zookeeper-server-start.sh config/zookeeper.properties & 
 bin/kafka-server-start.sh config/server.properties & 
 bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test
