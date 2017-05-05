8677749a6c2b8885828c3cc0524e57302a789a79
布小沫 <95112082@qq.com>
2017-05-05 14:04:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 sed -i "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/0.0.0.0:9092/g" config/server.properties
+
 # sed -i "s/#advertised.listeners/advertised.listeners/g" config/server.properties
 # sed -i "s/your.host.name:9092/${KAFKA_HOST_IP}:${KAFKA_PORT}/g" config/server.properties
+
+sed -i "/log.retention.hours/a message.max.byte=5242880" config/server.properties
+sed -i "/log.retention.hours/a default.replication.factor=2" config/server.properties
+sed -i "/log.retention.hours/a replica.fetch.max.bytes=5242880" config/server.properties
 IP=`ip addr | grep inet | grep -v "127.0.0.1" | awk -F '/' '{print $1}' | awk '{print $2}'`
 echo "host.name=${IP}" >>  config/server.properties
 if [ ${BROKER_ID} != "NULL" ];then
