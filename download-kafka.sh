7b500c049691f3f179b46684672c6ac3b210bc9b
布小沫 <95112082@qq.com>
2017-05-27 18:13:22 +0800

    Update download-kafka.sh

--- download-kafka.sh
+++ download-kafka.sh
@@ -1,4 +1,3 @@
-#!/bin/bash
 axel -n 20 --output=/usr/local/src/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz http://apache.fayea.com/kafka/${KAFKA_VERSION}/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz
 tar -zxf /usr/local/src/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz -C /usr/local/
 rm -rf /usr/local/src/kafka_${SCALA_VERSION}-${KAFKA_VERSION}.tgz
