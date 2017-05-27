7acecafd49718298b3b70b0d7c6bde0458544631
布小沫 <95112082@qq.com>
2017-05-28 00:49:32 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,13 +4,15 @@ IP=`hostname -i`
 
 sed -i "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/0.0.0.0:9092/g" config/server.properties
 
+# DCE
 sed -i "s/#advertised.listeners/advertised.listeners/g" config/server.properties
 sed -i "s/your.host.name:9092/${KAFKA_HOST_IP}:${KAFKA_PORT}/g" config/server.properties
+echo "host.name=${KAFKA_HOST_IP}" >>  config/server.properties
 
 # sed -i "s/#advertised.listeners/advertised.listeners/g" config/server.properties
 # sed -i "s/your.host.name:9092/${IP}:9092/g" config/server.properties
+# echo "host.name=${IP}" >>  config/server.properties
 
-echo "host.name=${IP}" >>  config/server.properties
 if [ ${BROKER_ID} != "NULL" ];then
     sed -i "s/broker.id=.*/broker.id=${BROKER_ID}/g" config/server.properties
 fi
