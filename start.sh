0333695b6decdc9ce608715a56fe6932f60f0f82
布小沫 <95112082@qq.com>
2017-06-20 02:06:24 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
-sed -i "s/zookeeper.connection.timeout.ms=6000/zookeeper.connection.timeout.ms=12000/g" config/server.properties
-sed -i "s/#listeners=PLAINTEXT:\/\/:9092/listeners=PLAINTEXT:\/\/0.0.0.0:9092/g" config/server.properties
-
-KAFKA_EXTERNAL=`echo ${KAFKA_EXTERNAL} | tr [A-Z] [a-z]`
-if [ ${KAFKA_EXTERNAL} == 'true' ];then
-    # DCE
-    sed -i "s/#advertised.listeners/advertised.listeners/g" config/server.properties
-    sed -i "s/your.host.name:9092/${KAFKA_HOST_IP}:${KAFKA_PORT}/g" config/server.properties
-    echo "host.name=${KAFKA_HOST_IP}" >>  config/server.properties
-elif [ ${KAFKA_EXTERNAL} == 'false' ];then
+# 本次测试，支持Docker之外的访问访问没有问题，暂未测试Docker内部调用
+# 根据环境变量判定是否让kafak支持Docker之外的服务访问
+# 若需要支持Docker之外的服务访问请设置KAFKA_HOST_IP,KAFKA_PORT这两个环境变量
+if [ ${KAFKA_HOST_IP} != "NULL" ] && [ ${KAFKA_PORT} != "NULL" ];then
+    echo "advertised.host.name=${KAFKA_HOST_IP}" >> config/server.properties
+    echo "advertised.port=${KAFKA_PORT}" >> config/server.properties
+else
     IP=`hostname -i`
     sed -i "s/#advertised.listeners/advertised.listeners/g" config/server.properties
     sed -i "s/your.host.name:9092/${IP}:9092/g" config/server.properties
     echo "host.name=${IP}" >>  config/server.properties
-else
-    echo "EXTERNAL parameter error,the parameter is true or false."
-    exit 1 
 fi
-
+# 集群必须设置此环境变量值
 if [ ${BROKER_ID} != "NULL" ];then
     sed -i "s/broker.id=.*/broker.id=${BROKER_ID}/g" config/server.properties
 fi
+# 设置zookeeper集群配置
 if [ ${ZOOKEEPER_CLUSTER} == "localhost:2181" ];then
-    bin/zookeeper-server-start.sh config/zookeeper.properties & 
-else 
+    bin/zookeeper-server-start.sh config/zookeeper.properties &
+else
     sed -i "s/zookeeper.connect=localhost:2181/zookeeper.connect=${ZOOKEEPER_CLUSTER}/g" config/server.properties
 fi
-
+# 启动服务后是否自动创建TOPICS
+if [ ${KAFKA_TOPICS} != "NULL" ];then
+    echo "create.topics=${KAFKA_TOPICS}" >> config/server.properties
+fi
 exec bin/kafka-server-start.sh config/server.properties
-
-# sleep 5
-# if [ ${KAFKA_TOPICS} != "NULL" ];then
-#    if [ ${ZOOKEEPER_CLUSTER} == "localhost:2181" ];then
-#        bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic ${KAFKA_TOPICS}
-#    else
-#        zookeeper=`echo $ZOOKEEPER_CLUSTER | sed "s/,/\n/g" | head -n 1`
-#        bin/kafka-topics.sh --create --zookeeper ${zookeeper} --replication-factor 1 --partitions 1 --topic ${KAFKA_TOPICS}
-#    fi
-# fi
-# exec tail -f logs/*.log
